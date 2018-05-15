56756e586ebaafc5bd7d5997d25fab9a8f08a5fc
Natasa Danic <danic.natasa@gmail.com>
2018-05-15 22:54:33 +0200

    Added main menu tests.

--- NewIkea.Framework/MainMenu.cs
+++ NewIkea.Framework/MainMenu.cs
@@ -15,5 +15,48 @@ namespace NewIkea.Framework
         {
             _driver.FindElement(By.Id("imgIKEALogoHeader")).Click();
         }
+
+        public void ClickOnInspirationLink()
+        {
+            _driver.FindElement(By.LinkText("INSPIRATION")).Click();
+        }
+
+        public void ClickOnThisIsIkeaLink()
+        {
+            _driver.FindElement(By.LinkText("THIS IS IKEA")).Click();
+        }
+
+        public void ClickOnNewLowerPrices()
+        {
+            _driver.FindElement(By.LinkText("NEW LOWER PRICES")).Click();
+        }
+
+        public void ClickOnProductLink()
+        {
+            _driver.FindElement(By.LinkText("PRODUCTS")).Click();
+        }
+
+        public bool IsProductDivVisible()
+        {
+            return _driver.FindElement(By.Id("menu-drawer-nh")).Displayed;
+        }
+
+        public void ClickOnRoomsLink()
+        {
+            _driver.FindElement(By.LinkText("ROOMS")).Click();
+        }
+
+        public bool IsRoomsDivVisible()
+        {
+            return _driver.FindElement(By.Id("rooms-drawer")).Displayed;
+        }
+
+        // not sure should it be one or tree methods
+        public void ClickOnSearchButton()
+        {
+            _driver.FindElement(By.Id("search")).Click();
+            _driver.FindElement(By.Id("search")).SendKeys("brown");
+            _driver.FindElement(By.XPath("//input[@alt='Search']")).Click();
+        }
     }
 }
--- NewIkea/MainMenyTests.cs
+++ NewIkea/MainMenyTests.cs
@@ -12,5 +12,53 @@ namespace NewIkea.Tests
 
             Assert.IsTrue(Pages.CurrentPageTitle == "IKEA | Affordable Swedish Home Furniture - IKEA");
         }
+
+        [Test]
+        public void InspirationLink_ShouldRedirectToInspirationPage()
+        {
+            Pages.MainMenu.ClickOnInspirationLink();
+
+            Assert.IsTrue(Pages.CurrentPageTitle == "IKEA + you");
+        }
+
+        [Test]
+        public void ThisIsIkeaLink_ShouldRedirectToThisIsIkeaPage()
+        {
+            Pages.MainMenu.ClickOnThisIsIkeaLink();
+
+            Assert.IsTrue(Pages.CurrentPageTitle == "This is IKEA - IKEA");
+        }
+
+        [Test]
+        public void NewLowerPricesLink_ShoulRedirectToNewLowerPricesPage()
+        {
+            Pages.MainMenu.ClickOnNewLowerPrices();
+
+            Assert.IsTrue(Pages.CurrentPageTitle == "New Lower Prices | IKEA Australia - IKEA");
+        }
+
+        [Test]
+        public void WhenClickedOnProductLink_ShouldShowProductsDropDownMenu()
+        {
+            Pages.MainMenu.ClickOnProductLink();
+
+            Assert.AreEqual(true, Pages.MainMenu.IsProductDivVisible());
+        }
+
+        [Test]
+        public void WhenClickOnRoomsLink_ShouldShowRoomsDropDownMenu()
+        {
+            Pages.MainMenu.ClickOnRoomsLink();
+
+            Assert.AreEqual(true, Pages.MainMenu.IsRoomsDivVisible());
+        }
+
+        [Test]
+        public void SearchButton_ShouldRedirectToSearchPage()
+        {
+            Pages.MainMenu.ClickOnSearchButton();
+
+            Assert.IsTrue(Pages.CurrentPageTitle == "Search result - IKEA");
+        }
     }
 }
